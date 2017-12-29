12f90f13c0a4e24b4dcd0b44629a88d742fc1b50
arennett <a.rennett@gmx.de>
2017-12-29 10:11:24 +0100

    .

--- XSerialTestClient/.ino.cpp
+++ XSerialTestClient/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-12-28 18:30:21
+//This file has been generated on 2017-12-29 09:08:50
 
 #include "Arduino.h"
 #include "Arduino.h"
@@ -13,7 +13,7 @@
 void setup() ;
 void loop() ;
 void onPreConnect(SerialNode* pNode) ;
-void onMessage(const tSerialHeader * pHeader,const byte* pData, size_t data_size,SerialNode* pNode) ;
+void onMessage(const tSerialHeader * pHeader, const byte* pData, 		size_t data_size, SerialNode* pNode) ;
 
 #include "XSerialTestClient.ino"
 
--- XSerialTestClient/XSerialTestClient.ino
+++ XSerialTestClient/XSerialTestClient.ino
@@ -4,62 +4,62 @@
 #include <SoftSerialPort.h>
 #include <SerialNode.h>
 
-
 SoftSerialPort* pSerialPort;
-
 
 tSerialHeader sheader;
 
-byte data[] = {55,99,88,44}; // some data
-SerialNode *pNode1,*pNode2;
-bool conditionsOK=false;
+byte data[] = { 55, 99, 88, 44 }; // some data
+SerialNode *pNode1, *pNode2;
+bool conditionsOK = false;
+bool bStart = false;
 
 unsigned long millis_start = millis();
 
-void setup()
-{
-	 Serial.begin(9600);
+void setup() {
+	Serial.begin(9600);
 
-	 MPRINTLNS("");
-	 MPRINTLNS("setup SerialTestMaster");
-	 SerialNode::init(11);
-	 pSerialPort=new SoftSerialPort(10,11,10);
-	 pSerialPort->begin(9600);
-	 pNode1  = SerialNode::createNode(1,false,10,1);
-	 pNode2  = SerialNode::createNode(2,false,10,2);
+	MPRINTLNS("");
+	MPRINTLNS("setup SerialTestMaster");
+	SerialNode::init(11);
+	pSerialPort = new SoftSerialPort(10, 11, 10);
+	pSerialPort->createDataBuffer(sizeof(unsigned long));
+	pSerialPort->begin(9600);
+	SerialNode::setOnPreConnectCallBack(onPreConnect);
+	SerialNode::setOnMessageCallBack(onMessage);
+	pNode1 = SerialNode::createNode(1, false, 10, 1);
+	pNode2 = SerialNode::createNode(2, false, 10, 2);
 
-	 XPRINTFREE;
+	XPRINTFREE
+	;
 }
 
-
-void loop()
-{
+void loop() {
 	SerialNode::processNodes();
 
-
 	if (!SerialNode::areAllNodesConnected()) {
+		bStart = false;
 		return;
+	} else {
+		bStart = true;
 	}
 
-	// system calibrated , and al l
-	MPRINTLNS("system calibrated and all nodes connected with remote");
-
-
-
+	if (bStart) {
+		MPRINTLNS("system calibrated and all nodes connected with remote");
+		bStart = false;
+	}
 
 }
 
-
-
-
 void onPreConnect(SerialNode* pNode) {
-	MPRINTLNSVAL("onPreConnect :" ,pNode->getId());
+	MPRINTLNSVAL("onPreConnect :", pNode->getId());
 	pNode->setReady(true);
 }
 
+void onMessage(const tSerialHeader * pHeader, const byte* pData,
+		size_t data_size, SerialNode* pNode) {
+	MPRINTLNSVAL("user message received :", pNode->getId());
+	MPRINTLNSVAL("user message data     :", *(unsigned long* )pData);
 
-void onMessage(const tSerialHeader * pHeader,const byte* pData, size_t data_size,SerialNode* pNode) {
-	MPRINTLNSVAL("user message received :" ,pNode->getId() );
-	 // send NAK;
+	// send NAK;
 }
 
